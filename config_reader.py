6c9c4797695a449c3a99db6efff81532a92da3e6
d.kozlov <d.kozlov@inspector-cloud.com>
2026-01-13 23:08:28 +0300

    add another config

--- config_reader.py
+++ config_reader.py
@@ -5,8 +5,8 @@ load_dotenv()
 
 
 class Config:
-    TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-    WEATHER_TOKEN = os.getenv("WEATHER_TOKEN")
+    TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "8556699634:AAGbYa1rKh-8XsKQLMagT1I9hJxBZZd7peI")
+    WEATHER_TOKEN = os.getenv("WEATHER_TOKEN", "23e4cb20d027f986ae78f2d7304ebc5f")
 
 
 config = Config()
